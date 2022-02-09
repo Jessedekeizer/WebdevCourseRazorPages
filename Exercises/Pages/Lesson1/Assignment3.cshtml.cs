902f01a4540476a24d62756d6c3e116def5faa0b
Jessedekeizer <jessedekeizer03@gmail.com>
2022-02-09 17:05:20 +0100

    oefenen

--- Exercises/Pages/Lesson1/Assignment3.cshtml.cs
+++ Exercises/Pages/Lesson1/Assignment3.cshtml.cs
@@ -1,12 +1,20 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Exercises.Pages.Lesson1
 {
     public class Assignment3 : PageModel
     {
-        public void OnGet()
-        {
+        
+        [BindProperty]
+        public int InputBindProperty {get; set;}
 
+        public int Output { get; set; }
+
+        public void OnPost([FromForm]string input)
+        {
+            Output = Convert.ToInt32(input);
         }
     }
 }
